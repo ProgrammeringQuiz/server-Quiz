c1eaf5198b1cfee2771cc878b13af2435943108d
Lukas Finne <lukas.ikvm1@gmail.com>
2022-10-22 17:54:24 +0200

    Add basic connection to MongoDB

--- server.js
+++ server.js
@@ -1,22 +1,48 @@
+const { MongoClient } = require('mongodb');
 
-var express = require('express');
-var app = express();
-var fs = require('fs');
-var cors = require('cors')
+async function main() {
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/drivers/node/ for more details
+     */
+    const uri = "mongodb+srv://admin:4zYSLmNp7iMBGV1oyDfS@cluster0.fldldow.mongodb.net/?retryWrites=true&w=majority";
 
-app.use(cors())
+    /**
+     * The Mongo Client you will use to interact with your database
+     * See https://mongodb.github.io/node-mongodb-native/3.6/api/MongoClient.html for more details
+     * In case: '[MONGODB DRIVER] Warning: Current Server Discovery and Monitoring engine is deprecated...'
+     * pass option { useUnifiedTopology: true } to the MongoClient constructor.
+     * const client =  new MongoClient(uri, {useUnifiedTopology: true})
+     */
+    const client = new MongoClient(uri);
 
-// Endpoint to Get a list of questions
-app.get('/getQuestions', function(req, res){
-    fs.readFile(__dirname + "/json/" + "quizData.json", 'utf8', function(err, data){
-        console.log(data);
-        res.json(JSON.parse(data));
-    });
-})
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
 
-// Create a server to listen at port 8080
-var server = app.listen(8080, function(){
-    var host = server.address().address
-    var port = server.address().port
-    console.log("REST API demo app listening at http://%s:%s", host, port)
-})
+        await findOneListingByName(client, "javascript")
+
+        // Make the appropriate DB calls
+
+    } finally {
+        // Close the connection to the MongoDB cluster
+        await client.close();
+    }
+
+}
+
+main().catch(console.error);
+
+// Add functions that make DB calls here
+async function findOneListingByName(client, nameOfListing) {
+    const result = await client.db("quiz").collection("java-questions").findOne({ title: nameOfListing });
+
+    if (result) {
+        console.log(`Found a listing in the collection with the name '${nameOfListing}':`);
+        console.log(result);
+    } else {
+        console.log(`No listings found with the name '${nameOfListing}'`);
+    }
+}
+
+
