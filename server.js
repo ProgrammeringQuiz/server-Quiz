7d72987ef491ef028c4f0f486067795ba6651457
Lukas Finne <lukas.ikvm1@gmail.com>
2022-10-23 12:07:04 +0800

    Add findAll query

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ async function main() {
         // Connect to the MongoDB cluster
         await client.connect();
 
-        await findOneListingByName(client, "javascript")
+        //await findListingsWithMinimumBedroomsBathroomsAndMostRecentReviews(client, "What is Java?")
+        await findListingsWithMinimumBedroomsBathroomsAndMostRecentReviews(client);
 
         // Make the appropriate DB calls
 
@@ -34,14 +35,32 @@ async function main() {
 main().catch(console.error);
 
 // Add functions that make DB calls here
-async function findOneListingByName(client, nameOfListing) {
-    const result = await client.db("quiz").collection("java-questions").findOne({ title: nameOfListing });
+async function findOneListingByName(client, question) {
+    const result = await client.db("quiz").collection("java-questions").find({ question: question });
 
     if (result) {
-        console.log(`Found a listing in the collection with the name '${nameOfListing}':`);
+        console.log(`Found a listing in the collection with the name '${question}':`);
         console.log(result);
     } else {
-        console.log(`No listings found with the name '${nameOfListing}'`);
+        console.log(`No listings found with the name '${question}'`);
+    }
+}
+
+async function findListingsWithMinimumBedroomsBathroomsAndMostRecentReviews(client) {
+    const cursor = client.db("quiz").collection("java-questions").find()
+
+    const results = await cursor.toArray();
+
+    if (results.length > 0) {
+        results.forEach((result, i) => {
+            console.log();
+            console.log(`${i + 1}. question: ${result.question}`);
+            console.log(`   _id: ${result._id}`);
+            console.log(`   answer: ${result.answer}`);
+            console.log(`   options: ${result.options}`);
+        });
+    } else {
+        console.log(`No listings found with at least bedrooms and bathrooms`);
     }
 }
 
