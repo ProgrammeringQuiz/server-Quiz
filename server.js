6d5f5ae017ba8502b2476ebe6879f6c7eeb85590
Lukas Finne <lukas.ikvm1@gmail.com>
2022-10-23 12:07:44 +0800

    Reformat code

--- server.js
+++ server.js
@@ -1,29 +1,20 @@
 const { MongoClient } = require('mongodb');
 
 async function main() {
-    /**
-     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-     * See https://docs.mongodb.com/drivers/node/ for more details
-     */
+
     const uri = "mongodb+srv://admin:4zYSLmNp7iMBGV1oyDfS@cluster0.fldldow.mongodb.net/?retryWrites=true&w=majority";
 
-    /**
-     * The Mongo Client you will use to interact with your database
-     * See https://mongodb.github.io/node-mongodb-native/3.6/api/MongoClient.html for more details
-     * In case: '[MONGODB DRIVER] Warning: Current Server Discovery and Monitoring engine is deprecated...'
-     * pass option { useUnifiedTopology: true } to the MongoClient constructor.
-     * const client =  new MongoClient(uri, {useUnifiedTopology: true})
-     */
     const client = new MongoClient(uri);
 
     try {
         // Connect to the MongoDB cluster
         await client.connect();
 
+        // Make the appropriate DB calls
         //await findListingsWithMinimumBedroomsBathroomsAndMostRecentReviews(client, "What is Java?")
         await findListingsWithMinimumBedroomsBathroomsAndMostRecentReviews(client);
 
-        // Make the appropriate DB calls
+
 
     } finally {
         // Close the connection to the MongoDB cluster
