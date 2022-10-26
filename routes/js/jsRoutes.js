8efb253dded06b19f371ca5eb6f38572d9ead7bc
Lukas Finne <lukas.ikvm1@gmail.com>
2022-10-26 05:24:32 +0200

    Add jsLoops.js

--- routes/js/jsRoutes.js
+++ routes/js/jsRoutes.js
@@ -8,10 +8,15 @@ const {
     getFunctionAll,
     createFunctionQuestion
 } = require("../../controllers/js/jsFunctionController");
+const {
+    getLoopsAll,
+    createLoopsQuestion
+} = require("../../controllers/js/jsLoopsController");
 
 const router = express.Router();
 
 router.route("/variable").get(getVariableAll).post(createVariableQuestion)
 router.route("/function").get(getFunctionAll).post(createFunctionQuestion)
+router.route("/loops").get(getLoopsAll).post(createLoopsQuestion)
 
 module.exports = router;
