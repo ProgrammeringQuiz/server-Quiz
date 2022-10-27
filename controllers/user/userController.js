0df39d31a62b0db277243e81ebfb3884e3452e57
Lukas Finne <lukas.ikvm1@gmail.com>
2022-10-27 06:42:40 +0200

    Add userController.js

---
+++ controllers/user/userController.js
@@ -0,0 +1,20 @@
+const getUserService = require("../../services/user/userService");
+
+
+exports.getUserAll = async (req, res) => {
+    try {
+        const quiz = await getUserService.getAll();
+        res.json({ data: quiz, status: "success" });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+exports.createUser = async (req, res) => {
+    try {
+        const quiz = await getUserService.createUser(req.body);
+        res.json({ data: quiz, status: "success" });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
