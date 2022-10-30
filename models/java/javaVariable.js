4f8bfb53181fbc01348f6ba630fa38b0d15fd8e8
Lukas Finne <lukas.ikvm1@gmail.com>
2022-10-30 16:02:16 +0100

    Add Images to java-variable

--- models/java/javaVariable.js
+++ models/java/javaVariable.js
@@ -4,9 +4,8 @@ const Schema = mongoose.Schema;
 const javaVariableSchema = new Schema({
     title: String,
     question: String,
-    img: {
-        data: Buffer,
-        contentType: String
+    questionImg: {
+        type: String
     },
     answer: Number,
     options: [String]
