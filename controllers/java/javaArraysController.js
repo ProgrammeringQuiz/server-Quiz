bacdbb9b46a2a4c6893a4c873f33d89c6c40ac03
Lukas Finne <lukas.ikvm1@gmail.com>
2022-11-02 07:50:24 +0100

    Add more question images

--- controllers/java/javaArraysController.js
+++ controllers/java/javaArraysController.js
@@ -1,4 +1,5 @@
 const javaArraysService = require("../../services/java/javaArraysService");
+const mongoose = require("mongoose");
 
 
 exports.getArraysAll = async (req, res) => {
@@ -12,7 +13,16 @@ exports.getArraysAll = async (req, res) => {
 
 exports.createArraysQuestion = async (req, res) => {
     try {
-        const quiz = await javaArraysService.createQuestion(req.body);
+        const question = ({
+            _id: new mongoose.Types.ObjectId(),
+            title: req.body.title,
+            question: req.body.question,
+            answer: req.body.answer,
+            options: req.body.options,
+            questionImg: req.file.path,
+        })
+
+        const quiz = await javaArraysService.createQuestion(question);
         res.json({ data: quiz, status: "success" });
     } catch (err) {
         res.status(500).json({ error: err.message });
